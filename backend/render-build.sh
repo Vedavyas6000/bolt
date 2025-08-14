492d5587e2023498ca4764c6c34e796a26aeffba
Vedanth <vedantmgali@gmail.com>
2025-08-14 19:07:53 +0530

    Initial CollegeBuzz project

---
+++ backend/render-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting backend build..."
+cd backend
+npm install
+echo "Backend build completed!"
--- backend/sql/add_id_document_path_column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE colleges ADD COLUMN id_document_path VARCHAR(255);
