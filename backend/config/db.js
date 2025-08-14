796e4c09c3b5a5c66609cf8a64dbb00d43732350
Vedanth <vedantmgali@gmail.com>
2025-08-15 01:06:51 +0530

    db done

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,21 +3,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'veda',
-  database: process.env.MYSQL_DATABASE || 'college_buzz'
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
+
 
 const connectDB = () => {
-  connection.connect(error => {
-    if (error) {
-      console.error('Error connecting to MySQL database:', error);
-      process.exit(1);
-    }
-    console.log('MySQL database connected');
-  });
+  connection.connect((err) => {
+  if (err) {
+    console.error('❌ MySQL connection error:', err);
+    process.exit(1);
+  }
+  console.log('✅ Connected to MySQL on Railway');
+});
 };
 
 module.exports = connectDB;
